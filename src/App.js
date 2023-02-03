e23d316948ca0d07adb8b8c4d28f3460c8afa369
prabhat1001 <prabhatsaraswat24@gmail.com>
2023-02-03 23:37:04 +0530

    updated: Routing established

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css';
 import Home from './Pages/Home';
 import Order from './Pages/OrderPage';
+import SignUp from './Components/SignUp';
 
 const App = () => {
   return (
     <Router>
         <Routes> 
+          {/* <Route exact path="/login" element={<Login />}></Route> */}
           <Route exact path="/" element={<Home />}></Route>
           <Route exact path="/order" element={<Order />}></Route>
+          <Route exact path="/signup" element={<SignUp />}></Route>
         </Routes>
       </Router>
   )
